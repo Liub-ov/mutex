d0ad55d092bd74b9a4106892f129f62e4ef9aae0
Liub-ov <79312905+Liub-ov@users.noreply.github.com>
2021-06-02 18:58:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp5
+{
+    public class MutexClass
+    {
+        public static int Count;
+        public static Mutex mutex = new Mutex();
+    }
+    public class Thread1
+    {
+        int num;
+        public Thread Thrd;
+
+        public Thread1(string name, int n)
+        {
+            Thrd = new Thread(this.Run);
+            num = n;
+            Thrd.Name = name;
+            Thrd.Start();
+        }
+        void Run()
+        {
+            Console.WriteLine(Thrd.Name + " wait for mutex");
+
+            MutexClass.mutex.WaitOne();
+
+            Console.WriteLine(Thrd.Name + " bring the mutex");
+
+            do
+            {
+                Thread.Sleep(500);
+                MutexClass.Count++;
+                Console.WriteLine("in thread {0}, Count={1}", Thrd.Name, MutexClass.Count);
+                num--;
+            } while (num > 0);
+
+            Console.WriteLine(Thrd.Name + " relieve the mutex ");
+
+            MutexClass.mutex.ReleaseMutex();
+        }
+    }
+    public class Thread2
+    {
+        int num;
+        public Thread Thrd;
+
+        public Thread2(string name, int n)
+        {
+            Thrd = new Thread(new ThreadStart(this.Run));
+            num = n;
+            Thrd.Name = name;
+            Thrd.Start();
+        }
+        void Run()
+        {
+            Console.WriteLine(Thrd.Name + " wait for mutex");
+
+            MutexClass.mutex.WaitOne();
+
+            Console.WriteLine(Thrd.Name + " bring the mutex");
+
+            do
+            {
+                Thread.Sleep(500);
+                MutexClass.Count--;
+                Console.WriteLine("in thread {0}, Count={1}", Thrd.Name, MutexClass.Count);
+                num--;
+            } while (num > 0);
+
+            Console.WriteLine(Thrd.Name + " relieve the mutex");
+
+            MutexClass.mutex.ReleaseMutex();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Thread1 mutex1 = new Thread1("Thread1", 1);
+            Thread.Sleep(1);
+
+            Thread2 mutex2 = new Thread2("Thread2", 0);
+
+            mutex1.Thrd.Join();
+            mutex2.Thrd.Join();
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using ConsoleApp5;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Thread1 mutex1 = new Thread1("Thread1", 3);
+            Thread.Sleep(1);
+            Thread2 mutex2 = new Thread2("Thread2", 2);
+            mutex1.Thrd.Join();
+            mutex2.Thrd.Join();
+            int expected = 1;
+            int result = MutexClass.Count;
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Thread1 mutex1 = new Thread1("Thread1", 5);
+            Thread.Sleep(1);
+            Thread2 mutex2 = new Thread2("Thread2", 5);
+            mutex1.Thrd.Join();
+            mutex2.Thrd.Join();
+            int expected = 1;
+            int result = MutexClass.Count;
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Thread1 mutex1 = new Thread1("Thread1", 1);
+            Thread.Sleep(1);
+            Thread2 mutex2 = new Thread2("Thread2", 0);
+            mutex1.Thrd.Join();
+            mutex2.Thrd.Join();
+            int expected = 1;
+            int result = MutexClass.Count;
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+
+}
